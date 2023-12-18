180a1720a79084aea4fb32eb857da131be1bc345
Massimo <massimo@massimo-HP-ENVY-Laptop-17-ce0xxx>
2023-12-18 22:29:19 +0000

    Added typer for CLI

--- src/{{ python_package_name }}/common/main.py
+++ src/{{ python_package_name }}/common/main.py
@@ -1,12 +1,23 @@
 """Docstring"""
+import typer
+from typing_extensions import Annotated
+
 from {{ python_package_name }}.api.something import a_cool_function
 from {{ python_package_name }}.common.utils import get_logger
 
 logger = get_logger(__name__)
+cli = typer.Typer(add_completion=False)
 
 
-def hello_world() -> str:
+@cli.command()
+def hello_world(
+        name: Annotated[str, typer.Option(help="Your name.")] = "cowboy"
+) -> str:
     """Hello."""
     a_cool_function()
-    logger.info("We are here")
-    return "We are here"
+    logger.info(f"We are here, {name}")
+    return f"We are here, {name}"
+
+
+if __name__ == '__main__':
+    cli()
