cdef15c283dcb33c6e04d004811bc3970d94e2bf
Massimo <massimo@massimo-HP-ENVY-Laptop-17-ce0xxx>
2023-12-18 22:45:56 +0000

    fixed test

--- tests/test_boilerplate.py
+++ tests/test_boilerplate.py
@@ -3,4 +3,4 @@ from {{python_package_name}}.common.main import hello_world
 
 class TestBoilerplate:
     def test_hello_world(self):
-        assert hello_world() == "We are here"
+        assert "We are here" in hello_world()
