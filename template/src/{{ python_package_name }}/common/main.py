98cba28ce2bff9b668cd6638f67ca76f464a23d8
massimo-lu <massimo@git.hub>
2024-01-06 15:21:01 +0000

    Removed something.py

--- template/src/{{ python_package_name }}/common/main.py
+++ template/src/{{ python_package_name }}/common/main.py
@@ -2,7 +2,6 @@
 import typer
 from typing_extensions import Annotated
 
-from {{ python_package_name }}.api.something import a_cool_function
 from {{ python_package_name }}.common.utils import get_logger
 
 logger = get_logger(__name__)
@@ -14,7 +13,6 @@ def hello_world(
         name: Annotated[str, typer.Option(help="Your name.")] = "cowboy"
 ) -> str:
     """Hello."""
-    a_cool_function()
     logger.info(f"We are here, {name}")
     return f"We are here, {name}"
 
